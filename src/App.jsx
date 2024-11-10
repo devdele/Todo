917151b8d491ff8dbc432835238fee17a79d5fa9
devdele <ayodeletoluwani71@gmail.com>
2024-11-10 16:04:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "./style.css"
+import { NewTodoForm } from "./NewTodoForm";
+
+export default function App () {
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+     })
+  };
+
+
+  // function handleSubmit (e)  {
+  //    e.preventDefault()
+
+  //    setTodos(currentTodos => {
+  //     return [
+  //       ...currentTodos,
+  //       { id: crypto.randomUUID(), title: newItem, completed: false },
+  //     ]
+  //    })
+
+  //   setNewItem('')
+  // }
+  
+  function toggleTodo (id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodos (id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+
+   } )
+}
+
+
+
+
+   return (
+    <>
+    <NewTodoForm onSubmit={addTodo} />
+   <h1 className="Header">Todo List</h1>
+   <ul className="list">
+    {todos.length === 0 && "No Todos"}
+    {todos.map(todo =>{
+      return (
+      <li key={todo.id}>
+      <label>
+       <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+       { todo.title }
+      </label>
+      <button 
+      onClick={() => deleteTodos(todo.id)} 
+      className="btn btn-danger"
+      >Delete</button>
+    </li>
+      )
+    })}
+   </ul>
+   </>
+   );
+};
